590d618ae4ace2d7a0c6d5c8b36b7226bf395fbe
Paolo Renzi <renzi.1887793@studenti.uniroma1.it>
2024-05-22 19:34:54 +0200

    more tests

--- src/experimental.py
+++ src/experimental.py
@@ -6,7 +6,7 @@ class BaseModel(nn.Module):
     def __init__(self, device:str, max_lenght:int, model_name:str = "microsoft/deberta-v3-base"):
         super(BaseModel, self).__init__()
         self.model = AutoModel.from_pretrained(model_name).to(device)
-        self.kan = KAN(width=[max_lenght*768, 3, 2], device=device)
+        self.kan = KAN(width=[max_lenght*768, 3], device=device)
 
     def forward(self, tokens):
         batch_size = tokens['input_ids'].shape[0]
@@ -15,7 +15,7 @@ class BaseModel(nn.Module):
 
         data = cls_output.view(batch_size, -1)
 
-        return self.kan()
+        return self.kan.forward(data)
 
 # MAIN
 if __name__ == '__main__' :
@@ -27,8 +27,7 @@ if __name__ == '__main__' :
     
     data = load_dataset("fever","v2.0",cache_dir="../data/fever", trust_remote_code=True)
     print("data loaded")
-    model = AutoModel.from_pretrained("microsoft/deberta-v3-base").to(device)
-    kan = KAN(width=[32*768, 3, 2], device=device)
+    model = BaseModel(device, 32, "microsoft/deberta-v3-base")
     tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-v3-base")
     print("model loaded")
     dataloader = DataLoader(data['validation'], batch_size=128, shuffle=True)
@@ -40,12 +39,6 @@ if __name__ == '__main__' :
         tokens = tokenizer(inputs, padding=True, truncation=True, return_tensors='pt',max_length=32)
         tokens.to(device)
         with torch.no_grad():
-           predictions = model(**tokens)
+           predictions = model(tokens)
 
-        cls_output = predictions.last_hidden_state
-
-        kan_data = cls_output.view(128, -1)
-
-        predictions = kan() 
-
-        print(predictions.shape)
+        print(predictions)
