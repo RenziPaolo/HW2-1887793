d62409301e564eac7aacec999c70c79e32653b01
RenziPaolo <paolorenzi00.1@gmail.com>
2024-05-23 07:21:12 +0200

    should work

--- src/experimental.py
+++ src/experimental.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from kan import KAN
 
 class BaseModel(nn.Module):
-    def __init__(self, device:str, max_lenght:int, model_name:str = "microsoft/deberta-v3-base"):
+    def __init__(self, device:str, max_lenght:int, model_name:str = "microsoft/mdeberta-v3-base"):
         super(BaseModel, self).__init__()
         self.model = AutoModel.from_pretrained(model_name).to(device)
         self.kan = KAN(width=[max_lenght*768, 3], device=device)
@@ -15,7 +15,7 @@ class BaseModel(nn.Module):
 
         data = cls_output.view(batch_size, -1)
 
-        return self.kan.forward(data)
+        return self.kan(data)
 
 # MAIN
 if __name__ == '__main__' :
@@ -27,18 +27,15 @@ if __name__ == '__main__' :
     
     data = load_dataset("fever","v2.0",cache_dir="../data/fever", trust_remote_code=True)
     print("data loaded")
-    model = BaseModel(device, 32, "microsoft/deberta-v3-base")
-    tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-v3-base")
+    model = BaseModel(device, 32, "microsoft/mdeberta-v3-base")
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/mdeberta-v3-base")
     print("model loaded")
     dataloader = DataLoader(data['validation'], batch_size=128, shuffle=True)
-
-    model.eval()
 
     for step, inputs in enumerate(dataloader):
         inputs = inputs['claim']
         tokens = tokenizer(inputs, padding=True, truncation=True, return_tensors='pt',max_length=32)
         tokens.to(device)
-        with torch.no_grad():
-           predictions = model(tokens)
+        predictions = model(tokens)
 
         print(predictions)
